8c338a5e3f9d032ffe96d33d3e2c24c977ad5d4c
tournamentsakamao-debug <tournamentsakamao@gmail.com>
2026-02-02 15:51:51 +0530

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -8,8 +8,8 @@ import { createClient } from '@supabase/supabase-js';
  */
 
 // 👇👇 YAHAN PASTE KARO
-const SUPABASE_URL = 'PASTE_SUPABASE_PROJECT_URL';
-const SUPABASE_ANON_KEY = 'PASTE_SUPABASE_ANON_PUBLIC_KEY';
+const SUPABASE_URL = 'https://tzymckfxeatibiuhvqwn.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6eW1ja2Z4ZWF0aWJpdWh2cXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzMzE3NTMsImV4cCI6MjA4NDkwNzc1M30.N6KxcDLi1o54RKEGf6ZDzj3J2t50gy0YAPMiEOJQ3yQ';
 
 // Supabase client
 export const supabase = createClient(
