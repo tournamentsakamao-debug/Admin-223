1e1a4f5795d4a1c82f710743b072b8a06e7832c6
tournamentsakamao-debug <tournamentsakamao@gmail.com>
2026-02-02 15:34:29 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
+// React root
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
